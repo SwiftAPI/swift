bfc6f76fefc10b498765d76a631fae855f6bad30
Buro26-Henri <henri@buro26.nl>
2022-07-16 09:43:51 +0200

    v0.2.01

--- src/Console/globals-and-includes.php
+++ src/Console/globals-and-includes.php
@@ -29,7 +29,6 @@ if (version_compare(PHP_VERSION, SWIFT_MINIMUM_PHP, '<')) {
 }
 
 require_once INCLUDE_DIR . '/vendor/autoload.php';
-require_once INCLUDE_DIR . '/src/php_namespaced/index.php';
 require_once 'ApplicationInterface.php';
-require_once INCLUDE_DIR . '/src/swift/Application/Bootstrap/Autoloading/Autoloader.php';
+require_once INCLUDE_DIR . '/vendor/swift-api/swift/src/Application/Bootstrap/Autoloading/Autoloader.php';
 require_once 'Bootstrap.php';
--- src/Runtime/Server/server-bootstrap.php
+++ src/Runtime/Server/server-bootstrap.php
@@ -8,8 +8,8 @@
  * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
  */
 
-require dirname( __DIR__, 4 ) . '/vendor/autoload.php';
-require_once dirname( __DIR__, 4 ) . '/src/swift/Runtime/Server/RuntimeApplication.php';
+require dirname( __DIR__, 6 ) . '/vendor/autoload.php';
+require_once 'RuntimeApplication.php';
 
 $CliApplication = new \Swift\Runtime\Server\RuntimeApplication();
 $CliApplication->run();
