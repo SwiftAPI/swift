55f29a80c266a2639bee76745e735697f0442f97
Buro26-Henri <henri@buro26.nl>
2022-07-30 15:42:07 +0200

    Add default timezone and deprecation settings to Middleware #45

---
+++ src/Kernel/Middleware/DefaultTimeZoneMiddleware.php
@@ -0,0 +1,38 @@
+<?php declare( strict_types=1 );
+
+/*
+ * This file is part of the Swift Framework
+ *
+ * (c) Henri van 't Sant <hello@henrivantsant.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Swift\Kernel\Middleware;
+
+use Swift\Configuration\Configuration;
+use Swift\Configuration\ConfigurationInterface;
+use Swift\DependencyInjection\Attributes\Autowire;
+
+#[Autowire]
+class DefaultTimeZoneMiddleware implements MiddlewareInterface {
+    
+    public function __construct(
+        protected ConfigurationInterface $configuration,
+    ) {
+    }
+    
+    public function getPriority(): int {
+        return KernelMiddlewarePriorities::TIMEZONE;
+    }
+    
+    public function process(
+        \Psr\Http\Message\ServerRequestInterface $request,
+        \Psr\Http\Server\RequestHandlerInterface $handler,
+    ): \Psr\Http\Message\ResponseInterface {
+        date_default_timezone_set( $this->configuration->get( 'app.timezone', 'root' ) ?? 'Europe/Amsterdam' );
+        
+        return $handler->handle( $request );
+    }
+    
+}
