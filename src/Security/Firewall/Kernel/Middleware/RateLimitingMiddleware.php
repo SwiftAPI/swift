560da29bef8cff2349fb0401f49ce9201071abd0
Buro26-Henri <henri@buro26.nl>
2022-07-30 15:47:45 +0200

    Prepare Rate Limiting Middleware #45

---
+++ src/Security/Firewall/Kernel/Middleware/RateLimitingMiddleware.php
@@ -0,0 +1,31 @@
+<?php declare( strict_types=1 );
+
+/*
+ * This file is part of the Swift Framework
+ *
+ * (c) Henri van 't Sant <hello@henrivantsant.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Swift\Security\Firewall\Kernel\Middleware;
+
+use Swift\Kernel\Middleware\KernelMiddlewarePriorities;
+use Swift\Kernel\Middleware\MiddlewareInterface;
+
+class RateLimitingMiddleware implements MiddlewareInterface {
+    
+    public function getPriority(): int {
+        return KernelMiddlewarePriorities::RATE_LIMIT;
+    }
+    
+    public function process(
+        \Psr\Http\Message\ServerRequestInterface $request,
+        \Psr\Http\Server\RequestHandlerInterface $handler,
+    ): \Psr\Http\Message\ResponseInterface {
+        
+        
+        return $handler->handle( $request );
+    }
+    
+}
